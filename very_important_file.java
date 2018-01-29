874a4fb4469d2caa4933dc1b0044bd624036fb07
przwojtkow <przemyslaw.wojtkow@capgemini.com>
2018-01-29 18:48:49 +0100

    work in progress

--- very_important_file.java
+++ very_important_file.java
@@ -1,19 +1,21 @@
 package top.secret.data
 
+
+
 public class Foo {
 	
-	private String name;
-	private String email;
-	private String pass;
-	private long motherInLawHateLevel;
+private String name;
+private String email;
+private String pass;
+private long motherInLawHateLevel;
 	
 	public bar() {
-		Stream<Pair> allFactorials = Stream.iterate(
-  new Pair(BigInteger.ONE, BigInteger.ONE),
-  x -> new Pair(
-    x.num.add(BigInteger.ONE),
-    x.value.multiply(x.num.add(BigInteger.ONE))));
-return allFactorials.filter(
-  (x) -> x.num.equals(num)).findAny().get().value;
+			Stream<Pair> allFactorials = Stream.iterate(
+	new Pair(BigInteger.ONE, BigInteger.ONE),
+								x -> new Pair(
+		x.num.add(BigInteger.ONE),
+		x.value.multiply(x.num.add(BigInteger.ONE))));
+			return allFactorials.filter(
+	(x) -> x.num.equals(num)).findOne().get().value;
+		}
 	}
-}
